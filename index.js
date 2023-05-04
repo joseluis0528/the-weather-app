16318a39ed9139c0697fcf0a0e799ebde266d13b
Jose <joseluis0528@gmail.com>
2023-05-03 21:23:15 -0500

    updated fetchData so that user can search by zipcode as well

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function fetchData(city) {
+function fetchData(city, zipcode) {
     const apiKey = 'c364d9671c097b51f7d4c53a27ed9951';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
-    .then(responce => responce.json())
-    .then(data => console.log(data))
+    if(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+        .then(responce => responce.json())
+        .then(data => console.log(data))
+    } else if(zipcode) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apiKey}&units=imperial`)
+        .then(responce => responce.json())
+        .then(data => console.log(data))
+    }
 }
