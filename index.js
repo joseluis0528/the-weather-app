7717a7bc1961e2ce2ef24c1ece35ba660e66f60b
Jose <joseluis0528@gmail.com>
2023-05-04 21:27:30 -0500

    added event listener to search button

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+handleClick()
+})
+
 function fetchData(city, zipcode) {
     const apiKey = 'c364d9671c097b51f7d4c53a27ed9951';
     if(city) {
@@ -7,6 +11,13 @@ function fetchData(city, zipcode) {
     } else if(zipcode) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apiKey}&units=imperial`)
         .then(responce => responce.json())
-        .then(data => console.log(data))
+        .then(data => renderData(data))
     }
+}
+
+function handleClick() {
+    const searchButton = document.querySelector('#search-button');
+    searchButton.addEventListener('click', () => {
+        console.log('click')
+    })
 }
