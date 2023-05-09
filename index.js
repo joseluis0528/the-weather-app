e2e3f30044f0a4e42de60bab7fa9c1e36aea948d
Jose <joseluis0528@gmail.com>
2023-05-08 22:53:09 -0500

    restructured date to get day, month and year only

--- index.js
+++ index.js
@@ -57,4 +57,9 @@ function renderData(data) {
 
 function getDate() {
     const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    document.querySelector('#date').textContent = `${day}/${month}/${year}`
 }
